b22a10b124109ede002d419a0b609bcad0feacc6
Zantron2000 <xanderpalmer7@gmail.com>
2025-09-15 09:53:09 -0400

    Modernized the footer

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # StudyBee
+
+## To Dos
+
+1. Ensure when mobile header menu is open, clicking outside closes it.
+2. Ensure when subjects header is open, clicking outside closes it.
+3. Ensure home page "Create your own study sets" SVGs are vertically centered with text on mobile.
+4. Fix home images on shrinking to have better resolution.
+5. Fix footer links to match up rows.
+6. Swap the secondary and tertiary button colors and buttons assigned those classes.
+7. Replace tertiary passive border color to just border color and remove instances of border-passive class.
+8. Update study guide page flashcard buttons to not move when index increases / decreases.
+9. Update the study guide title to work with very long titles.
+10. Ensure that when activity header menu is open, clicking outside closes it.
+11. Make the finished flashcards page buttons and text better centered.
+12. Move all card terms over to use the shared component.
+13. Center the flashcard in the screen on the flashcard page
+14. Add z-index for subjects so it can prioritize.
+15. Make flashcards page mobile friendly.
+16. Ensure max length of terms and definitions handled properly in all pages.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,51 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Footer() {
     const footerLinks = [
-        { group: 'About Us', links: [{ name: 'About StudyBee', href: '#' }, { name: 'Donate', href: '#' }, { name: 'Our Mission', href: '#' }] },
-        { group: 'Users', links: [{ name: 'Learn', href: '#' }, { name: 'Teachers', href: '#' }, { name: 'Live', href: '#' }, { name: 'Import Profile', href: '#' }] },
-        { group: 'Support', links: [{ name: 'GitHub', href: '#' }, { name: 'Contribute', href: '#' }] },
+        { group: 'Users', links: [{ name: 'Study modes', href: '#' }, { name: 'Teachers', href: '#' }, { name: 'Live', href: '#' }, { name: 'Import Profile', href: '#' }] },
+        { group: 'Support', links: [{ name: 'GitHub', href: '#' }, { name: 'Contribute', href: '#' }, { name: 'Donate', href: '#' }] },
         { group: 'Resources', links: [{ name: 'Help Center', href: '#' }, { name: 'Community Guidelines', href: '#' }, { name: 'Privacy & Terms', href: '#' }, { name: 'Cookie Settings', href: '#' }] },
     ]
 
     return (
-        <footer>
-            <div className="bg-background-secondary text-foreground-secondary pt-4 px-16 flex justify-between pb-16">
-                {
-                    footerLinks.map((section) => (
-                        <div key={section.group}>
-                            <div className="text-lg font-bold mb-2">
-                                {section.group}
-                            </div>
-                            <div className="space-y-1 text-md">
-                                {section.links.map((link) => (
-                                    <div key={link.name}>
-                                        <a href={link.href} className="hover:underline">
-                                            {link.name}
-                                        </a>
-                                    </div>
-                                ))}
-                            </div>
+        <footer className="bg-secondary text-secondary-foreground flex justify-center">
+            <div className="px-8 max-w-7xl w-full">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-8 py-8">
+                    <div className="text-lg font-semibold">
+                        <div className="flex justify-center md:justify-start items-center mb-4">
+                            <Image className="mr-2" src={'/StudyBeeLogo-light.png'} alt="StudyBee Logo" width={30} height={20} />
+                            <div>StudyBee</div>
                         </div>
-                    ))
-                }
-            </div>
-            <div className="bg-background-quaternary text-foreground-quaternary py-4 px-16">
-                <p className="text-xl">
-                    &copy;StudyBee 2024. All rights reserved.
-                </p>
+                        <div className="text-sm text-secondary-foreground-passive font-normal w-full text-center md:text-left">
+                            The best way to study with flashcards. Join millions of students and educators worldwide.
+                        </div>
+                    </div>
+                    {
+                        footerLinks.map((section) => (
+                            <div key={section.group}>
+                                <div className="text-center md:text-left text-md font-bold mb-2 lg:mb-4">
+                                    {section.group}
+                                </div>
+                                <div className="text-center md:text-left space-y-2 md:space-y-1 text-sm">
+                                    {section.links.map((link) => (
+                                        <div key={link.name}>
+                                            <a href={link.href} className="hover:text-secondary-foreground text-secondary-foreground-passive">
+                                                {link.name}
+                                            </a>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="bg-secondary text-secondary-foreground-passive border-t-1 border-secondary-border py-8">
+                    <p className="text-sm text-center">
+                        &copy;2025 StudyBee. All rights reserved.
+                    </p>
+                </div>
             </div>
         </footer>
     );
